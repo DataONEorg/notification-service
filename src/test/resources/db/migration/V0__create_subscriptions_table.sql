37ccb2ef10d21a98f7d0ed8c6a951276828a287c
Matthew B <106352182+artntek@users.noreply.github.com>
2025-08-12 16:56:26 -0700

    de-dupe flyway migrations

--- src/test/resources/db/migration/V0__create_subscriptions_table.sql
+++
@@ -1,8 +0,0 @@
---- Create the Notifications Service "Subscriptions" Table
-CREATE TABLE subscriptions
-(
-    id            SERIAL PRIMARY KEY,
-    resource_type VARCHAR(255),
-    subject       VARCHAR(512),
-    pid           VARCHAR(512)
-);
