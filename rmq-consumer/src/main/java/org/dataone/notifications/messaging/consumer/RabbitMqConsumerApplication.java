8d3084a24a8b62cb5b43af034d3531ea8523577c
Matthew B <106352182+artntek@users.noreply.github.com>
2025-12-10 14:08:58 -0800

    reorg

--- rmq-consumer/src/main/java/org/dataone/notifications/messaging/consumer/RabbitMqConsumerApplication.java
+++ rmq-consumer/src/main/java/org/dataone/notifications/messaging/consumer/RabbitMqConsumerApplication.java
@@ -2,8 +2,6 @@ package org.dataone.notifications.messaging.consumer;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import jakarta.ejb.Singleton;
-import jakarta.ejb.Startup;
 import org.dataone.notifications.NsConfig;
 import org.dataone.notifications.messaging.config.RabbitMqConnectionManager;
 import org.dataone.notifications.messaging.config.RabbitMqProperties;
@@ -13,8 +11,6 @@ import org.slf4j.LoggerFactory;
 /**
  * Bootstraps the RabbitMQ consumer alongside the Jakarta EE runtime.
  */
-@Startup
-@Singleton
 public class RabbitMqConsumerApplication {
 
     private static final Logger log = LoggerFactory.getLogger(RabbitMqConsumerApplication.class);
